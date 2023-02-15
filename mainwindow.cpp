4f498700b0efdfe2a917e9468fce54195cfb6aef
liufeng4080 <liufeng4080@163.com>
2023-02-15 21:58:59 +0800

    period scan after 10 min, and start tx after 60 min

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7931,10 +7931,10 @@ void MainWindow::on_the_minute ()
   else { txwatchdog (false); }
   //3...4 minutes to stop AP decoding
   if(!m_transmitting && m_mode=="FT8" && (m_jtdxtime->currentMSecsSinceEpoch2()-m_mslastTX) > 120000) m_lapmyc=0;
-  // if no TX in lastest 5 minutes, switch/scan tx period, every minute
-  if(m_autoTx && !m_transmitting && m_mode=="FT8" && (m_jtdxtime->currentMSecsSinceEpoch2()-m_mslastTX) > 300000) ui->TxMinuteButton->click ();
-  // if no TX in lastest 10 minutes, tx
-  if(m_autoTx && !m_transmitting && m_mode=="FT8" && (m_jtdxtime->currentMSecsSinceEpoch2()-m_mslastTX) > 600000) enableTx_mode(true);
+  // if no TX in lastest 10 minutes, switch/scan tx period, every minute
+  if(m_autoTx && !m_transmitting && m_mode=="FT8" && (m_jtdxtime->currentMSecsSinceEpoch2()-m_mslastTX) > 600000) ui->TxMinuteButton->click ();
+  // if no TX in lastest 60 minutes, tx
+  if(m_autoTx && !m_transmitting && m_mode=="FT8" && (m_jtdxtime->currentMSecsSinceEpoch2()-m_mslastTX) > 3600000) enableTx_mode(true);
 }
 
 void MainWindow::toggle_skipTx1 ()
